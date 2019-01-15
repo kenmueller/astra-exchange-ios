d69904f3f45658b15ca885b72d7f9d607f60ee37
Ken Mueller <kenmueller0@gmail.com>
2019-01-14 16:24:30 -0800

    Initial Commit

---
+++ Astra Exchange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Astra Exchange
+//
+//  Created by Ken Mueller on 1/14/19.
+//  Copyright © 2019 Ken Mueller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ Astra ExchangeTests/Astra_ExchangeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Astra_ExchangeTests.swift
+//  Astra ExchangeTests
+//
+//  Created by Ken Mueller on 1/14/19.
+//  Copyright © 2019 Ken Mueller. All rights reserved.
+//
+
+import XCTest
+@testable import Astra_Exchange
+
+class Astra_ExchangeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
