8475b2b11f3d3e25c4430b984ae05776b51d035f
Ken Mueller <kenmueller0@gmail.com>
2019-01-15 22:16:22 -0800

    loading view works

--- Astra Exchange/UserViewController.swift
+++ Astra Exchange/UserViewController.swift
@@ -3,7 +3,9 @@ import CoreData
 import Firebase
 
 class UserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-	@IBOutlet weak var balanceLabel: UILabel!
+	@IBOutlet weak var loadingView: UIView!
+	@IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+	@IBOutlet weak var offlineView: UIView!
 	@IBOutlet weak var actionsTableView: UITableView!
 	
 	struct Action {
@@ -19,6 +21,8 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		if startup {
+			loadingView.isHidden = false
+			activityIndicator.startAnimating()
 			ref.child("users").observe(.childAdded) { snapshot in
 				users.append(User(id: snapshot.key, name: retrieveDataValue(snapshot: snapshot, field: "name") as! String, email: retrieveDataValue(snapshot: snapshot, field: "email") as! String))
 			}
@@ -35,13 +39,16 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
 								ref.child("users/\(id!)/name").observeSingleEvent(of: .value) { snapshot in
 									name = snapshot.value as? String
 									self.navigationItem.title = name
+									email = localEmail
+									loadData()
+									self.activityIndicator.stopAnimating()
+									self.loadingView.isHidden = true
 								}
-								email = localEmail
-								loadData()
 							} else if let error = error {
 								switch error.localizedDescription {
-								case "FIRAuthErrorCodeNetworkError":
-									self.showAlert("No internet")
+								case "Network error (such as timeout, interrupted connection or unreachable host) has occurred.":
+									self.activityIndicator.stopAnimating()
+									self.offlineView.isHidden = false
 								default:
 									self.showHomeVC()
 								}
@@ -59,10 +66,7 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
-		updateChangeHandler {
-			self.balanceLabel.text = String(balance)
-			self.actionsTableView.reloadData()
-		}
+		actionsTableView.reloadData()
 	}
 	
 	func showHomeVC() {
@@ -82,14 +86,16 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
 	}
 	
 	func numberOfSections(in tableView: UITableView) -> Int {
-		return actions.count
+		return actions.count + 1
 	}
 	
 	func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
 		switch section {
 		case 0:
-			return "ACTIONS"
+			return "BALANCE"
 		case 1:
+			return "ACTIONS"
+		case 2:
 			return "LOOKUP"
 		default:
 			return nil
@@ -97,25 +103,53 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return actions[section].count
+		return section == 0 ? 1 : actions[section - 1].count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-		cell.textLabel?.text = actions[indexPath.section][indexPath.row].name
-		cell.detailTextLabel?.text = nil
-		if indexPath.section == 0, indexPath.row == 0 {
-			let unpaidInvoices = invoices.filter { $0.to == id! }
-			if unpaidInvoices.count == 1 {
-				cell.detailTextLabel?.text = "⚠️ 1 unpaid invoice"
-			} else if unpaidInvoices.count > 1 {
-				cell.detailTextLabel?.text = "⚠️ \(unpaidInvoices.count) unpaid invoices"
+		if indexPath.section == 0 {
+			cell.textLabel?.font = UIFont(name: "Nunito-SemiBold", size: 45)
+			updateChangeHandler { change in
+				if change == .balance {
+					UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+						cell.textLabel?.transform = CGAffineTransform(scaleX: 2, y: 2)
+						cell.textLabel?.alpha = 0.4
+						cell.textLabel?.text = String(balance)
+					}) { finished in
+						if finished {
+							UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+								cell.textLabel?.transform = CGAffineTransform.identity
+								cell.textLabel?.alpha = 1
+							}, completion: nil)
+						}
+					}
+				}
 			}
+			cell.textLabel?.text = String(balance)
+			cell.detailTextLabel?.text = nil
+			cell.accessoryType = .none
+			return cell
+		} else {
+			cell.textLabel?.font = UIFont(name: "Nunito-SemiBold", size: 18)
+			cell.detailTextLabel?.font = UIFont(name: "Nunito-SemiBold", size: 18)
+			cell.detailTextLabel?.textColor = .red
+			cell.textLabel?.text = actions[indexPath.section - 1][indexPath.row].name
+			cell.detailTextLabel?.text = nil
+			if indexPath.section == 0, indexPath.row == 0 {
+				let unpaidInvoices = invoices.filter { $0.to == id! }
+				if unpaidInvoices.count == 1 {
+					cell.detailTextLabel?.text = "⚠️ 1 unpaid invoice"
+				} else if unpaidInvoices.count > 1 {
+					cell.detailTextLabel?.text = "⚠️ \(unpaidInvoices.count) unpaid invoices"
+				}
+			}
+			return cell
 		}
-		return cell
 	}
 	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		performSelector(onMainThread: actions[indexPath.section][indexPath.row].action, with: nil, waitUntilDone: false)
+		if indexPath.section == 0 { return }
+		performSelector(onMainThread: actions[indexPath.section - 1][indexPath.row].action, with: nil, waitUntilDone: false)
 	}
 }
